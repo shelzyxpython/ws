a73a0aca3bd9797b5d47ddde002f03d2994002f6
shelzyxpython <144206667+shelzyxpython@users.noreply.github.com>
2023-10-02 13:44:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,88 @@
+number = int(input())
+for i in range(3):
+    print(number + i)
+
+#a,b,c = int(input()),int(input()),int(input())
+#print(a + b + c)
+
+#a = int(input())
+#print('Обьем =', a ** 3)
+#print('Площадь полной поверхности =', 6*a**2)
+
+#a = int(input())
+#b = int(input())
+#print(3*(a + b)**3 + 275 * b**2 -127 * a - 41)
+
+#a = int(input())
+#print('Следующее за числом', a, 'число', a + 1 )
+#print('Для числа',a,'предыдущее число', a - 1)
+
+#monitor,block,keyboard,mouse = int(input()),int(input()),int(input()),int(input())
+#print((monitor+block+keyboard+mouse)*3)
+
+#a1 = int(input())
+#d = int(input())
+#n = int(input())
+#an = a1 + d*(n-1)
+#print(an)
+
+#a = int(input())
+#print(a, 2 * a, 3 * a, 4 * a, 5 * a, sep = '---')
+
+#tangerine = int(input())
+#students = int(input())
+#print(students // tangerine)
+#print(students % tangerine)
+
+#people = int(input())
+#if people % 2 == 1:
+#    print(people // 2 + 1)
+#else:
+#    print(people // 2)
+
+#minutes = int(input())
+#print('150 минут - это', minutes // 60, 'час', minutes - (minutes // 60) * 60, 'минут' )
+
+#num = int(input())
+#num_1 = num // 100
+#num_2 = num % 100 // 10
+#num_3 = num % 10
+#print('Сумма цифр =', num_1 + num_2 + num_3)
+#print('Произведение цифр =', num_1 * num_2 * num_3)
+#n = int(input())
+#print('   _~_    ' * n)
+#print('  (o o)   ' * n)
+#print(' /  V  \  ' * n)
+#print('/(  _  )\ ' * n)
+#print('  ^^ ^^   ' * n)
+
+#num = int(input())
+#if num < 10:
+#    print(num % 10)
+#else:
+#    print(num // 10)
+
+#minutes = int(input())
+#print(minutes // 60, minutes - (minutes // 60) * 60)
+
+#num = int(input())
+#print(num * 1)
+
+#num = int(input())
+#if num % 2 == 1:
+#    print(num + 1)
+#else:
+#    print(num + 2)
+
+#v = int(input())
+#t = int(input())
+#road_1 = 109
+#road_2 = v * t
+#print(road_2 - road_1)
+
+#h = int(input())
+#a = int(input())
+#b = int(input())
+#print((h - a) // (a - b) + 1)
+
+
